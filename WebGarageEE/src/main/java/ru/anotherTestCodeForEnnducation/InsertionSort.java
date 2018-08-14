0425ddfacd3f54c1059a611854f610f6cf072ee0
Glor2m0 <bmp19@mail.ru>
2018-08-14 18:36:55 +0400

    update sort

--- WebGarageEE/src/main/java/ru/anotherTestCodeForEnnducation/InsertionSort.java
+++ WebGarageEE/src/main/java/ru/anotherTestCodeForEnnducation/InsertionSort.java
@@ -2,15 +2,16 @@ package ru.anotherTestCodeForEnnducation;
 
 public class InsertionSort {
 
-    void insertionSort (int[] a, int n){
+    void insertionSort (int[] a, int length){
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<length; i++){
                 int tmp = a[i];
             int j = i - 1;
-                for( j = i -1 ; j >=0 && tmp < a[j]; j--){
+                for( ; j >=0 && tmp < a[j]; j--){
                     a[j + i] = a[j];
                 }
                 a[j +  1] = tmp;
+
         }
     }
 
