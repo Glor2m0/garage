940ed53427337fe48c1b7ae23f8ab2898660848e
Glor2m0 <bmp19@mail.ru>
2018-08-16 20:44:32 +0400

    minor changes

--- WebGarageEE/src/main/java/ru/anotherTestCodeForEnnducation/RunnerTestClass.java
+++ WebGarageEE/src/main/java/ru/anotherTestCodeForEnnducation/RunnerTestClass.java
@@ -1,9 +1,22 @@
 package ru.anotherTestCodeForEnnducation;
 
 public class RunnerTestClass {
+
     public static void main(String[] args) {
        SomeHashF s = new SomeHashF();
-       System.out.print(s.gornersHash("sejkfgjhjnew", 5015));
+      // System.out.print(s.gornersHash("sejkfgjhjnew", 5015));
+        long a = 2345l;
+        int b ;
+        String re = "0123456";
+            String[] ssa = new String[2];
+        String sa;
+        sa = Long.toString(a);
+        b = Character.getNumericValue(sa.charAt(0));
+        if (re =="0123456")
+       System.out.println(re.substring(1,4));
+
+
+
 
     }
 }
