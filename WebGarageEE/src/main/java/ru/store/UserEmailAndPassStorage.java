dbf32aa2721c664a9194482ba511ea8cc1c8f7fe
Glor2m0 <bmp19@mail.ru>
2018-09-11 13:02:11 +0300

    Z project

--- WebGarageEE/src/main/java/ru/store/UserEmailAndPassStorage.java
+++ WebGarageEE/src/main/java/ru/store/UserEmailAndPassStorage.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class UserEmailAndPassStorage  {
-
+    //Z project
     final Connection connection;
 
     public UserEmailAndPassStorage() {
